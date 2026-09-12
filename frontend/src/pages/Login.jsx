13a643f518b3393f72efa9bba317698680baeeb8
sudarshan-sh <mail2sudarshan.sharma@gmail.com>
2026-09-12 11:54:05 +0530

    min height changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -35,7 +35,7 @@ const Login = ({ setUser }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-[80vh] bg-gray-100">
       <form
         action="#"
         onSubmit={handleSubmit}
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -37,7 +37,7 @@ const Signup = ({ setUser }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-[80vh] bg-gray-100">
       <form
         action="#"
         onSubmit={handleSubmit}
